ca5f142a5c14fae74705e0f03510cd26bf004003
Ana Iglesias <ana.iglesias.molina@gmail.com>
2023-08-25 20:54:42 +0200

    new plots workflow, data updated and reorganized

--- results/time-plot.py
+++ results/time-plot.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import math
 
-plt.style.use('bmh')
+plt.style.use('seaborn-whitegrid')
 
 
 def organize_data(data, technique, mappingsEG_TS):
@@ -26,7 +26,7 @@ def organize_data(data, technique, mappingsEG_TS):
 
 def plot(data, scale):
     # Create dataframe
-    technique = ['Reification', 'RDF-star', 'Named-graphs', 'N-ary']
+    technique = ['Std. Reif.', 'RDF-star', 'Named Graphs', 'N-Ary Rel.']
     mappingsEG_TS = ['Oxigraph', 'Fuseki', 'GraphDB', 'Morph-KGC', 'SPARQL-Anything']
     data = np.transpose(data)
     ordered_data = organize_data(data, [x.lower() for x in technique], mappingsEG_TS)
@@ -39,28 +39,25 @@ def plot(data, scale):
     r3 = [x + barWidth * 2 for x in r1]
     r4 = [x + barWidth * 3 for x in r1]
     r5 = [x + barWidth * 4 for x in r1]
-    # r6 = [x + barWidth * 5 for x in r1]
-    # r7 = [x + barWidth * 6 for x in r1]
-    # r9 = [x + barWidth * 8 for x in r1]
-    # r10 = [x + barWidth * 9 for x in r1]
 
     plt.bar(r1, ordered_data.values.tolist()[0], width=barWidth, color='#FC9F5B',  # F2BABA
-            label='Oxigraph')
+            label='Oxigraph', edgecolor = "gray")
     plt.bar(r2, ordered_data.values.tolist()[1], width=barWidth, color='#FBD1A2',  # A46593
-            label='Fuseki', )
+            label='Fuseki', edgecolor = "gray")
     plt.bar(r3, ordered_data.values.tolist()[2], width=barWidth, color='#ECE4B7',  # B1ACAA
-            label='GraphDB')
+            label='GraphDB', edgecolor = "gray")
     plt.bar(r4, ordered_data.values.tolist()[3], width=barWidth, color='#7DCFB6',  # B1ACAA
-            label='Morph-KGC')
+            label='Morph-KGC', edgecolor = "gray")
     plt.bar(r5, ordered_data.values.tolist()[4], width=barWidth, color='#33CA7F',  # 2862CC
-            label='SPARQL-Anything')
+            label='SPARQL-Anything', edgecolor = "gray")
+
 
     plt.xticks([r + barWidth * 1.5 for r in range(len(r1))], map(lambda each: each.strip(""), technique),
                fontsize=12)
     plt.yticks(np.arange(0, 6), ('0', '1', '2', '3', '4', 'TO'), fontsize=14)
-    plt.ylim(top=5)
+    plt.ylim(top=5,bottom=-1)
     plt.ylabel("Time (log$_{10}$(s))", fontsize=15)
-    plt.xlabel("Format", fontsize=15)
+    #plt.xlabel("Format", fontsize=15)
     plt.title(scale, fontsize=20)
     plt.legend(mappingsEG_TS, loc='lower center', ncol=6, bbox_to_anchor=[0.5, -0.26], fontsize=13)
     # plt.legend(engines, loc='lower center', prop={'size': 10}, ncol=6, bbox_to_anchor=(0.5, -0.28))
@@ -76,16 +73,9 @@ def plot(data, scale):
 
 
 def handler():
-    # scales = ['GTFS-1', 'GTFS-10', 'GTFS-100', 'GTFS-1000']
     scales = ['1K','10K', '100K', '1M']
     for scale in scales:
         plot(pd.read_csv("./" + scale + ".csv"), scale)
-        # if scale == 'GTFS-100':
-        #     plot100(pd.read_csv("./" + scale + ".csv"), scale)
-        # elif scale == 'GTFS-1000':
-        #     plot1000(pd.read_csv("./" + scale + ".csv"), scale)
-        # else:
-        #     plot(pd.read_csv("./"+ scale + ".csv"), scale)
 
 
 if __name__ == "__main__":
